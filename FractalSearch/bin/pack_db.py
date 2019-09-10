4a2adb9a2f8b1ef9905efafca8d4d8aa598b7326
NovaIscariot <enin10@inbox.ru>
2019-09-10 19:21:07 +0300

    done

--- FractalSearch/bin/pack_db.py
+++ FractalSearch/bin/pack_db.py
@@ -3,40 +3,46 @@ from bin.domen_collection import DomenCollection
 from bin.table import Table
 import bin.methods as cfg
 
-conn = pyodbc.connect('Driver={SQL Server};'
+
+def pack(table_name):
+    conn = pyodbc.connect('Driver={SQL Server};'
                           'Server=' + cfg.server +
                           ';Database=' + cfg.db +
                           ';Trusted_Connection=yes;')
-domen_collection = DomenCollection(Table("TestTable1"))
-cursor = conn.cursor()
+    domen_collection = DomenCollection(Table(table_name))
+    cursor = conn.cursor()
 
-sql_command = "CREATE TABLE packed \
-(id int IDENTITY(1,1) PRIMARY KEY,\n"
+    sql_command = "CREATE TABLE " + table_name + "_packed \
+    (id int IDENTITY(1,1) PRIMARY KEY,\n"
 
-domens = domen_collection.opt_collection.domens
+    domens = domen_collection.opt_collection.domens
 
-for i in range(len(domens)):
-    sql_command += "domen" + str(i+1) + " int"
-    if i != len(domens)-1:
-        sql_command += ",\n"
-    else:
-        sql_command += ")"
+    for i in range(len(domens)):
+        sql_command += "domen" + str(i + 1) + " int"
+        if i != len(domens) - 1:
+            sql_command += ",\n"
+        else:
+            sql_command += ")"
 
-rows = domen_collection.opt_collection.pack()
-cursor.execute(sql_command)
-conn.commit()
-for x in rows:
-    sql_command = "INSERT INTO packed VALUES " + x
+    rows = domen_collection.opt_collection.pack()
     cursor.execute(sql_command)
-conn.commit()
-conn.close()
+    conn.commit()
+    for x in rows:
+        sql_command = "INSERT INTO " + table_name + "_packed VALUES " + x
+        cursor.execute(sql_command)
+    conn.commit()
+    conn.close()
 
-output_f = open("F:\\DB_CourseWork\\FractalSearch\\bin\\dicts.txt", "w")
-for domen in domen_collection.opt_collection.domens:
-    for x in domen.atr_idx:
-        output_f.write(str(x)+" ")
-    output_f.write("\n")
-    for x in domen.dict:
-        output_f.write(" ".join(x)+"\n")
-    output_f.write('---\n')
-output_f.close()
+    output_f = open("F:\\DB_CourseWork\\FractalSearch\\bin\\"+table_name+"dicts.txt", "w")
+    for domen in domen_collection.opt_collection.domens:
+        for x in domen.atr_idx:
+            output_f.write(str(x) + " ")
+        output_f.write("\n")
+        for x in domen.dict:
+            output_f.write(" ".join(str(x)) + "\n")
+        output_f.write('---\n')
+    output_f.close()
+
+
+base_table_name = "TestCrime1"
+pack(base_table_name)
