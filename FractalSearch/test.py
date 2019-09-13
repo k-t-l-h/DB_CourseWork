3e10c8ab15a939b8dc29156d88dde286f7abc44c
NovaIscariot <enin10@inbox.ru>
2019-09-13 16:01:55 +0300

    test

--- FractalSearch/test.py
+++ FractalSearch/test.py
@@ -2,10 +2,13 @@ import time
 import bin.pack_db as pack
 import bin_mod.pack_db as pack_mod
 
+table_name = "TestTable"
+max_dom = 3
+
 tb = time.process_time()
-pack.pack("TestTable", 3)
+pack.pack(table_name, max_dom)
 tm = time.process_time()
 print(tm-tb)
-pack_mod.pack("TestTable", 3)
+pack_mod.pack(table_name, max_dom)
 te = time.process_time()
 print(te-tm)
